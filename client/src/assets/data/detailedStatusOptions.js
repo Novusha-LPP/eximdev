4f121f7d1f8849209030784bae89f80e5666b556
Atul Nagose <react@novusha.com>
2025-01-29 19:00:37 +0530

    DSR add

--- client/src/assets/data/detailedStatusOptions.js
+++ client/src/assets/data/detailedStatusOptions.js
@@ -14,26 +14,31 @@ export const detailedStatusOptions = [
   },
   {
     id: 6,
+    name: "Rail Out",
+    value: "Rail Out",
+  },
+  {
+    id: 7,
     name: "BE Noted, Arrival Pending",
     value: "BE Noted, Arrival Pending",
   },
   {
-    id: 7,
+    id: 8,
     name: "BE Noted, Clearance Pending",
     value: "BE Noted, Clearance Pending",
   },
   {
-    id: 8,
+    id: 9,
     name: "PCV Done, Duty Payment Pending",
     value: "PCV Done, Duty Payment Pending",
   },
   {
-    id: 9,
+    id: 10,
     name: "Cus.Clearance Completed, delivery pending",
     value: "Custom Clearance Completed",
   },
   {
-    id: 10,
+    id: 11,
     name: "Billing Pending",
     value: "Billing Pending",
   },

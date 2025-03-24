9f34d5240b25f927248dd4e862efc313f47f6a06
Atul Nagose <react@novusha.com>
2025-03-24 16:00:13 +0530

    fix: change modal, route, ui

--- server/model/srcc/Directory_Management/UnitMeasurementModal.mjs
+++ server/model/srcc/Directory_Management/UnitMeasurementModal.mjs
@@ -1,28 +1,18 @@
 import mongoose from "mongoose";
 
-const unitMeasurementSchema = new mongoose.Schema({
-  name: { type: String, trim: true },
+const measurementSchema = new mongoose.Schema({
+  unit: { type: String, trim: true },
   symbol: { type: String, trim: true },
-  unit_type: {
-    type: String,
-    enum: [
-      "number",
-      "weight",
-      "distance",
-      "volume",
-      "area",
-      "time",
-      "pressure",
-      "power",
-      "temperature",
-    ],
-    required: true,
-  },
   decimal_places: {
     type: Number,
     min: 0,
     default: 2,
   },
+});
+
+const unitMeasurementSchema = new mongoose.Schema({
+  name: { type: String, trim: true }, // e.g., "Length"
+  measurements: [measurementSchema],
 });
 
 export default mongoose.model("UnitMeasurement", unitMeasurementSchema);

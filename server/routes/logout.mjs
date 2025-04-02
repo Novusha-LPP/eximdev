fc121db224a7f15b9bd2e91bc09bae426f932fb6
Atul Nagose <react@novusha.com>
2025-04-02 13:47:30 +0530

    fix:

--- server/routes/logout.mjs
+++ server/routes/logout.mjs
@@ -5,14 +5,14 @@ const router = express.Router();
 router.post("/api/logout", (req, res) => {
   res.clearCookie("exim_token", {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    secure: false,
+    sameSite: "lax",
   });
 
   res.clearCookie("exim_user", {
     httpOnly: false,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    secure: false,
+    sameSite: "lax",
   });
 
   return res.status(200).json({ message: "Logged out successfully" });

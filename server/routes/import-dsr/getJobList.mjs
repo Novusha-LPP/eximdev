286092890a33bd3ce4da9323333284f7a9206bb0
rauf@intern <intern@novusha.com>
2025-04-10 12:28:26 +0530

    feat: Update environment configuration to use 'development' mode and refine field selection in getJobList

--- server/routes/import-dsr/getJobList.mjs
+++ server/routes/import-dsr/getJobList.mjs
@@ -25,9 +25,9 @@ const parseDate = (dateStr) => {
 
 // Field selection logic
 const defaultFields = `
-  job_no job_date year invoice_number invoice_date inv_currency total_inv_value unit_price description job_net_weight importer custom_house awb_bl_no awb_bl_date container_nos vessel_berthing 
-  gateway_igm_date discharge_date detailed_status be_no be_date loading_port  arrival_date free_time detention_from  container_number container.size weight_shortage weight_excess assessment_date  
-  port_of_reporting type_of_b_e consignment_type shipping_line_airline bill_date out_of_charge pcv_date  document_received_date  do_validity delivery_date emptyContainerOffLoadDate do_completed do_validity rail_out_date cth_documents payment_method supplier_exporter gross_weight job_net_weight
+  job_no year importer custom_house awb_bl_no container_nos vessel_berthing 
+  gateway_igm_date discharge_date detailed_status be_no be_date loading_port free_time
+  port_of_reporting type_of_b_e consignment_type shipping_line_airline bill_date out_of_charge pcv_date delivery_date emptyContainerOffLoadDate do_completed do_validity rail_out_date cth_documents payment_method supplier_exporter gross_weight job_net_weight processed_be_attachment ooc_copies gate_pass_copies
 `;
 
 const additionalFieldsByStatus = {

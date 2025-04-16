661e6e276e687c2a8e0f366bb1c4a1fc7994b204
Uday Zope <163131972+ComputerVisionAlluvium@users.noreply.github.com>
2025-04-16 19:26:45 +0530

    fix: update cookie settings for access and refresh tokens in logout route

--- server/routes/logout.mjs
+++ server/routes/logout.mjs
@@ -7,17 +7,21 @@ router.post("/api/logout", (req, res) => {
 
   // Clear access token
   res.clearCookie("access_token", {
+
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production", // Conditional based on environment
-    sameSite: "lax",
+    secure: true,
+    sameSite: "strict",
+    domain:".alvision.in",
     path: "/",
+    
   });
 
   // Clear refresh token
   res.clearCookie("refresh_token", {
     httpOnly: true, // Match the setting used when creating the cookie
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    secure: true,
+    sameSite: "strict",
+    domain:".alvision.in",
     path: "/",
   });
 

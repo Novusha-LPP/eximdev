af4c9cc6c57781acb7113749472549e373fb3026
ComputerVisionAlluvium <uday@alluvium.in>
2025-05-17 11:30:50 +0530

    remove the feature of downloading json when uploading excel

--- client/src/customHooks/useFileUpload.js
+++ client/src/customHooks/useFileUpload.js
@@ -277,17 +277,9 @@ function useFileUpload(inputRef, alt, setAlt) {
       }
 
       // Create debug download file
-      const blob = new Blob([JSON.stringify(modifiedData, null, 2)], {
-        type: "application/json",
-      });
-      const url = URL.createObjectURL(blob);
-      const link = document.createElement("a");
-      link.href = url;
-      link.download = "debug-job-upload.json";
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-      URL.revokeObjectURL(url);
+     
+    
+  
 
       await uploadAndCheckStatus(modifiedData);
     } catch (error) {

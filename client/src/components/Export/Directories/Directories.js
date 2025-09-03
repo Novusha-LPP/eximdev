94b920ac48cd50dc125512550b09775d2241909a
rauf-alluviam <intern@novusha.com>
2025-09-03 14:15:51 +0530

    setup done

--- client/src/components/Export/Directories/Directories.js
+++ client/src/components/Export/Directories/Directories.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Box, Typography, TextField, Paper, MenuItem } from "@mui/material";
+import { viewMasterList } from "../../assets/data/DirectoriesData.js";
+import DirectoryComponent from "./DirectoryComponent";
 
-const Directories = () => {
+function Directories() {
+  const [selectedDirectory, setSelectedDirectory] = useState("");
+
+  const handleDirectoryChange = (event) => {
+    console.log("Selected value:", event.target.value);
+    setSelectedDirectory(event.target.value);
+  };
+
   return (
-    <div>Directories</div>
-  )
+    <Box sx={{ p: 3 }}>
+      <Typography variant="h5" sx={{ mb: 3, fontWeight: "bold" }}>
+        Directory Management
+      </Typography>
+
+      <Paper sx={{ p: 2, mb: 3 }}>
+        <TextField
+          select
+          size="small"
+          label="Select Directory"
+          value={selectedDirectory}
+          onChange={handleDirectoryChange}
+          sx={{ minWidth: 250 }}
+        >
+          {viewMasterList.map((dir) => (
+            <MenuItem key={dir} value={dir}>
+              {dir}
+            </MenuItem>
+          ))}
+        </TextField>
+      </Paper>
+
+      {selectedDirectory && (
+        <DirectoryComponent directoryType={selectedDirectory} />
+      )}
+    </Box>
+  );
 }
 
-export default Directories
+export default React.memo(Directories);
